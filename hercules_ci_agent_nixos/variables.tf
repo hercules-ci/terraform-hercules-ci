45a1815edf320a13aa83acf84bf5108e5d3d2056
Robert Hensing <robert@roberthensing.nl>
2020-07-17 15:39:41 +0200

    Terraform 0.12 syntax

--- hercules_ci_agent_nixos/variables.tf
+++ hercules_ci_agent_nixos/variables.tf
@@ -1,12 +1,12 @@
 
 variable "use_prebuilt" {
-  type = "string"
+  type = string
   description = "Get agent binaries from the hercules-ci cache"
   default = true
 }
 
 variable "target_host" {
-  type = "string"
+  type = string
   description = "NixOS host that will have an agent configuration applied to it. Must be reachable through SSH."
 }
 
@@ -23,17 +23,17 @@ variable "build_on_target" {
 }
 
 variable "configs" {
-  type = "list"
+  type = list
   description = "Extra NixOS configuration modules to import"
 }
 
 variable "triggers" {
-  type = "map"
+  type = map
   description = "Extra variables for Terraform to trigger deployment"
 }
 
 variable "cluster_join_token" {
-  type = "string"
+  type = string
   description = "The cluster join token contents. See https://docs.hercules-ci.com/hercules-ci/reference/agent-config/#clusterJoinTokenPath"
 }
 
